cbb8b12ead0494caae5e9d0e1029c4adafc0fc5f
Vuong Ngo <info@vuongngo.com>
2022-01-06 22:27:33 +0100

    add try catch blocks

--- controller/orders.controller.js
+++ controller/orders.controller.js
@@ -6,23 +6,54 @@ const ordersServiceInstance = new ordersService();
 
 
 router.get('/', function(req, res) {
-  res.status(200).json(ordersServiceInstance.getAll());
+  try {
+    res.status(200).json(ordersServiceInstance.getAll());
+  } catch (err) {
+    res.status(500).send({
+      message: `An error occured. ${err}`,
+    });
+  }
 });
 
-router.get('/:id', function(req, res) {
-  res.status(200).json(ordersServiceInstance.getById(parseInt(req.params.id)));
+router.get('/:id', function(req, res) {  
+  try {
+    res.status(200).json(ordersServiceInstance.getById(parseInt(req.params.id)));
+  } catch (err) {
+    res.status(500).send({
+      message: `An error occured. ${err}`,
+    });
+  }
 });
 
 router.post('/', function(req, res) {
-  res.status(200).json(ordersServiceInstance.create(req.body));
+  try {
+    res.status(200).json(ordersServiceInstance.create(req.body));
+  } catch (err) {
+    res.status(500).send({
+      message: `An error occured. ${err}`,
+    });
+  }
 });
 
 router.put('/:id', function(req, res) {
-  res.status(200).json(ordersServiceInstance.update(parseInt(req.params.id), req.body));
+  try {
+    res.status(200).json(ordersServiceInstance.update(parseInt(req.params.id), req.body));
+  } catch (err) {
+    res.status(500).send({
+      message: `An error occured. ${err}`,
+    });
+  }  
 });
 
 router.delete('/:id', function(req, res) {
-  res.status(200).json(ordersServiceInstance.delete(parseInt(req.params.id)));
+  try {
+    res.status(200).json(ordersServiceInstance.delete(parseInt(req.params.id)));
+  } catch (err) {
+    res.status(500).send({
+      message: `An error occured. ${err}`,
+    });
+  }  
 });
+
 
 module.exports = router;
